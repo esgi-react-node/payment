3828428d022fbb2855f3f367f170d39f663f2b31
Lucas Lavander <xz351@hotmail.fr>
2020-07-14 00:33:48 +0200

    Update checkout page

--- server/routes/proccess.js
+++ server/routes/proccess.js
@@ -7,8 +7,10 @@ router.get('/:id', async (req, res) => {
   const transaction = await Transaction.findByPk(req.params.id);
   const {validUrl, cancelUrl} = req.query;
   transaction.amount = transaction.amount / 100;
+  const cart = transaction.cart.map(el => ({...el, price: el.price/100}))
   return res.render('payment', {
     transaction,
+    cart,
     validUrl,
     cancelUrl
   })
